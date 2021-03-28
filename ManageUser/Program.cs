5de781fbcb2657d7073f0440b3e7e7a800d43be3
Part_lo15 <jump-rostov@mail.ru>
2021-03-28 13:59:39 +0300

    Добавлено пространство именн RegistrationAccount и добавлен класс Registration

--- ManageUser/Program.cs
+++ ManageUser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RegistrationAccount;
 
 namespace ManageUser
 {
@@ -7,6 +8,11 @@ namespace ManageUser
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            
+            Registration registration = new Registration("mysql60.hostland.ru", "host1323541_itstep35", "host1323541_itstep", "269f43dc");
+            
+            registration.InsertNewItemInDate("jump-rostov@mail.ru", "anton2781618");
         }
     }
 }
---
+++ RegistrationAccount/Registration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace RegistrationAccount
+{
+    
+    public class Registration
+    {
+        private string host = "mysql60.hostland.ru";
+        private string dateBase = "host1323541_itstep";
+        private string userName = "host1323541_itstep35";
+        private string password = "269f43dc";
+
+        public Registration(){ }
+
+        //кноструктор базы данных
+        public Registration(string host, string dateBase, string userName, string password)
+        {
+            this.host = host;
+            this.dateBase = dateBase;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        //метод для передачи сигнатуры базы данных 
+        public void SetDateBaseInfo(string host, string dateBase, string userName, string password)
+        {
+            this.host = host;
+            this.dateBase = dateBase;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        
+        
+        
+        //показать всю таблицу
+        public void SelectOllItemsInDate()
+        {
+            ExecuteARequest("SELECT * FROM accounts;");
+        }
+        
+        //создать строку в таблице
+        public void InsertNewItemInDate(string email, string password)
+        {
+            ExecuteARequest($"INSERT INTO accounts VALUES (null, '{email}', '{password}');");
+        }
+        
+        //запрос в базу данных
+        private void ExecuteARequest(string request)
+        {
+            string connectionString = "Server=" + host + ";" + "Database=" + dateBase + ";" + "Uid=" + userName + ";" + "Pwd=" + password + ";charset=utf8;";
+
+           
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {                
+                connection.Open();               
+                if (!connection.Ping()) { Console.WriteLine("ошибка"); return; }
+              
+                var result = new MySqlCommand { Connection = connection, CommandText = request }.ExecuteReader();
+                
+               
+            }
+        }
+        
+        
+    }
+}
